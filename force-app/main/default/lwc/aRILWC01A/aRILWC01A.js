6c261b1e1e831b51e698b00e0af1b5725cceae33
rkzw20 <rkzw20@gmail.com>
2020-11-28 16:50:13 +0900

    copy HelloWOrldLightningWebComponent html, js, js-meta.xml

--- force-app/main/default/lwc/aRILWC01A/aRILWC01A.js
+++ force-app/main/default/lwc/aRILWC01A/aRILWC01A.js
@@ -1,3 +1,7 @@
-import { LightningElement } from 'lwc';
-
-export default class ARILWC01A extends LightningElement {}
+import { LightningElement } from "lwc";
+export default class ARILWC01A extends LightningElement {
+  greeting = "World";
+  changeHandler(event) {
+    this.greeting = event.target.value;
+  }
+}
