7c241b3108bf1bbd978c28ec3e1f745647bdca3d
rkzw20 <rkzw20@gmail.com>
2020-12-05 07:43:26 +0900

    push Apex hello world

--- force-app/main/default/lwc/aRILWC01A/aRILWC01A.js
+++ force-app/main/default/lwc/aRILWC01A/aRILWC01A.js
@@ -1,7 +1,9 @@
-import { LightningElement } from "lwc";
+import { LightningElement, wire, track } from "lwc";
+import sayHello from "@salesforce/apex/HelloWorld01.sayHello";
 export default class ARILWC01A extends LightningElement {
   greeting = "ARI";
   changeHandler(event) {
     this.greeting = event.target.value;
   }
+  @wire(sayHello) greetings;
 }
